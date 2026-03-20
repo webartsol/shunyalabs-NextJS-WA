97f60a9064f1a68f3b7ac3695e58d54b87ffeda7
webartsol <shalesh@webartsol.com>
2026-03-20 21:51:31 +0530

    1

--- src/app/blog/code-switching-asr-explained/layout.tsx
+++ src/app/blog/code-switching-asr-explained/layout.tsx
@@ -6,6 +6,34 @@ export const metadata: Metadata = {
     alternates: {
         canonical: "https://www.shunyalabs.ai/blog/code-switching-asr-explained",
     },
+	icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Getting Started with ASR APIs | Python Quickstart Guide",
+    description: "Most voice technology was built for clean, single-language speech and struggles the moment someone mixes Hindi and English or any other language",
+    url: "https://www.shunyalabs.ai/",
+    siteName: "Shunya Labs",
+    images: [
+      {
+        url: "/logo-light.png",
+        width: 1200,
+        height: 630,
+        alt: "Shunya Labs",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Getting Started with ASR APIs | Python Quickstart Guide",
+    description: "Most voice technology was built for clean, single-language speech and struggles the moment someone mixes Hindi and English or any other language",
+    images: ["/logo-light.png"],
+    creator: "@ShunyaLabs",
+  },
 };
 
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
